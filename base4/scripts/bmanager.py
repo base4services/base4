b3bff07cc8967a77964572ec36e39626ca342583
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-11-13 14:10:39 +0100

    ws

--- base4/scripts/bmanager.py
+++ base4/scripts/bmanager.py
@@ -89,7 +89,7 @@ def is_git_dirty(repo_path='.'):
 @click.option('--base-lib-update', '-u', is_flag=True, help='Update base4 library')
 @click.pass_context
 def do(ctx, new_service, reset_service, compile_env, compile_yaml, gen, pip_up, pip_down, fmt, ls_templates, template, base_lib_update):
-	print([new_service, reset_service, compile_env, compile_yaml, pip_up, pip_down, fmt, ls_templates, template, base_lib_update])
+	print([new_service, reset_service, compile_env, compile_yaml, gen, pip_up, pip_down, fmt, ls_templates, template, base_lib_update])
 	if not any([new_service, reset_service, compile_env, compile_yaml, pip_up, pip_down, fmt, ls_templates, template, base_lib_update]):
 		click.echo(ctx.get_help())
 		return
