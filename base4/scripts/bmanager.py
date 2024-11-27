3e7c5b2617c9cdf034d52d4c4806b0d24a2f3d78
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-11-27 21:05:10 +0100

    n

--- base4/scripts/bmanager.py
+++ base4/scripts/bmanager.py
@@ -145,7 +145,7 @@ def new_service(service_name, service_template, verbose, gen_type):
                 mkdir -p {project_root}/src/services/{service_name}
                 git clone git+ssh://git@github2/base4services/base4service_template.git {v}
                 cp -R base4service_template/services/template/* {project_root}/src/services/{service_name}
-                cp base4service_template/rename.sh {project_root}/src/services/{service_name}
+                cp base4service_template/rename.sh {project_root}/src
                 
                 cp base4service_template/tests/test_template.py {project_root}/tests/test_{service_name}.py 
                 cp base4service_template/tests/test_api_v2.py {project_root}/tests/test_api_v2_{service_name}.py
