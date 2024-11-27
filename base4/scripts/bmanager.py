c774a5be5ee652c2d77491fb7b54e5b8a04e8d69
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-11-28 00:19:02 +0100

    n

--- base4/scripts/bmanager.py
+++ base4/scripts/bmanager.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import sys
 from pathlib import Path
-
+import xml.etree.ElementTree as ET
 # import asyncclick as click
 import click
 import git
@@ -284,234 +284,69 @@ def _compile_yaml(yaml_file: str, service_name: str, gen_type: str):
 def compile_yaml(yaml_file: str, service_name: str, gen_type: str):
     _compile_yaml(yaml_file, service_name, gen_type)
 
-            # run test for new service
-            # os.system(f'pytest -n 8 --disable-warnings {project_root + f"/tests/test_{new_service}.py"} --no-cov')
 
+@do.command('pycharm')
+def create_pycharm_project_config():
+    """
+    Creates XML configuration for PyCharm, setting multiple source directories and a test directory.
+    Validates directories before processing.
 
-
-
-
-#
-# @click.command(context_settings=dict(help_option_names=['-h', '--help'], max_content_width=150))
-# @click.option('--command', '-c',
-#               type=click.Choice(
-#                   ['new-service',
-#                    'reset-service',
-#                    'compile-env',
-#                    'compile-yaml',
-#                    # 'gen-from-yaml',
-#                    'pip-up',
-#                    'pip-down',
-#                    'list-templates',
-#                    'base-lib-update',
-#                    'fmt',
-#                    'test',
-#                    'services',
-#                    'aerich'
-#                    ]),
-#               required=True,
-#               help='Command to execute',
-#               )
-# @click.option('--aerich', '-a', help='aerich command to execute')
-# @click.option('--service-name', '-s', help='Service name to generate or reset')
-# @click.option('--service-template', '-t', default='base4service_template', help='See list of templates with `bmanager -c list-templates')
-# @click.option('--yaml-file', '-y', default='gen.yaml', help='YAML file to use for generation')
-# @click.option('--gen-type', '-g', default='models,schemas', help='Components to generate (comma-separated: models,schemas,tables)')
-# @click.option('--verbose', '-v', is_flag=True, default=False, help='Verbose output')
-#
-# def do(command, aerich, service_name, yaml_file, service_template, gen_type, verbose):
-#
-#     #TODO: ovo pokupiti listanjem git repo-a
-#     existing_service_templates = ['base4tenants', 'base4ws', 'base4sendmail', 'base4service_template']
-#     v = '> /dev/null 2>&1' if verbose else ''
-#
-#     if command == 'base-lib-update':
-#         print('[*] Updating base4 library...')
-#         os.system(f'''cd {project_root}/lib/base4 && git pull''')
-#         return
-#
-#     if command == 'fmt':
-#         os.system(f'black --target-version py312 --line-length 160 --skip-string-normalization {project_root}')
-#         os.system(f'isort {project_root} --profile black --line-length 160')
-#         return
-#
-#     if command == 'services':
-#         print(f'[*] Available services:')
-#         for i, j in enumerate(get_service_names(), start=1):
-#             print(j)
-#         return
-#
-#     if command == 'aerich':
-#         if not aerich:
-#             print(f'[*] please provide aerich command')
-#             return
-#
-#         if aerich not in ('init', 'init-db', 'migrate', 'upgrade', 'downgrade'):
-#             print(f'[*] please provide valid aerich command')
-#             return
-#
-#         for service in ['aerich'] + get_service_names():
-#             if service_name and service != service_name:
-#                 continue
-#             print('aerich --app '+service+' '+aerich)
-#
-#     if command == 'test':
-#         os.system(
-#             f'''
-#         cd {project_root}
-#         TEST_DATABASE=sqlite pytest -n 8 --disable-warnings tests --no-cov
-#         cd -
-#         '''
-#         )
-#         return
-#
-#     if command == 'pip-down':
-#         return p_down()
-#
-#     if command == 'pip-up':
-#         return p_up()
-#
-#     if command == 'compile-env':
-#         yaml_to_env('env')
-#         print(f'[*] {project_root}/.env configuration generated!')
-#         return
-#
-#     if command == 'list-templates':
-#         for i, j in enumerate(existing_service_templates, start=1):
-#             print(f'->: {j}')
-#         return
-#
-#     if command == 'reset-service':
-#
-#         if not service_name:
-#             print(f'[*] please provide service name')
-#             return
-#
-#         os.system('git checkout .')
-#         try:
-#             shutil.rmtree(project_root + f'/src/services/{service_name}')
-#             os.remove(project_root + f'/tests/test_{service_name}.py')
-#             sys.exit(f'[*] service -> {service_name} files are reset.')
-#         except Exception as e:
-#             pass
-#         return
-#
-#     if command == 'new-service':
-#
-#         if not service_name:
-#             print(f'[*] please provide service name')
-#             return
-#
-#         # check is service already exists
-#         directory = Path(project_root + f'/src/services/{service_name}')
-#         if directory.is_dir():
-#             sys.exit(f'[*] service -> {service_name} already exists')
-#
-#         if is_git_dirty():
-#             print(f'[*] please commit previous changes!')
-#             os.system('git status')
-#             return
-#
-#         # if service_template:
-#         #     if service_template not in existing_service_templates:
-#         #         print(f'[*] please choose template')
-#         #         for i, j in enumerate(existing_service_templates, start=1):
-#         #             print(f'->: {j}')
-#         #         return
-#         #
-#         #     if service_template == 'base4tenants':
-#         #         os.system(
-#         #             f'''
-# 		# 		mkdir -p {project_root}/src/services/{service_name}
-# 		# 		git clone git+ssh://git@github2/base4services/base4tenants.git {v}
-# 		# 		cp -R base4tenants/src/services/tenants/* {project_root}/src/services/{service_name}/
-# 		# 		cp -R base4tenants/tests/test_base_tenants.py {project_root}/tests/
-# 		# 		cp -R base4tenants/tests/test_tenants.py {project_root}/tests/test_{service_name}.py
-# 		# 		cp -R base4tenants/rename.sh {project_root}/src/services/{service_name}
-# 		# 		rm -rf base4tenants
-#         #         cd {project_root}/src/services/{service_name}
-# 		# 		bash rename.sh {service_name}
-# 		# 		rm  {project_root}/src/services/{service_name}/rename.sh
-# 		# 		mv {project_root}/src/services/{service_name}/yaml_sources/tenants_model.yaml {project_root}/src/services/{service_name}/yaml_sources/{service_name}_model.yaml
-# 		# 		mv {project_root}/src/services/{service_name}/yaml_sources/tenants_schema.yaml {project_root}/src/services/{service_name}/yaml_sources/{service_name}_schema.yaml
-# 		# 		mv {project_root}/src/services/{service_name}/api/tenants.py {project_root}/src/services/{service_name}/api/{service_name}.py
-# 		# 		mv {project_root}/src/services/{service_name}/schemas/tenants.py {project_root}/src/services/{service_name}/schemas/{service_name}.py
-# 		# 		'''
-#         #         )
-#         #     elif service_template == 'base4ws':
-#         #         os.system(
-#         #             f'''
-# 		# 		git clone git+ssh://git@github2/base4services/base4ws.git {v}
-# 		# 		cp -R base4ws/ws {project_root}/src
-# 		# 		rm -rf base4ws
-# 		# 		'''
-#         #         )
-#         #     elif service_template == 'base4sendmail':
-#         #         os.system(
-#         #             f'''
-# 		# 		mkdir -p {project_root}/src/services/sendmail
-# 		# 		git clone git+ssh://git@github2/base4services/base4sendmail.git {v}
-# 		# 		cp -R sendmail/* {project_root}/src/services/sendmail
-# 		# 		rm -rf sendmail
-# 		# 		'''
-#         #         )
-#         #     elif service_template == 'base4service_template':
-#         #         print('[*] creating service from default template...')
-#         #         os.system(
-#         #             f'''
-# 		# 		mkdir -p {project_root}/src/services/{service_name}
-# 		# 		git clone git+ssh://git@github2/base4services/base4service_template.git {v}
-# 		# 		cp -R base4service_template/services/template/* {project_root}/src/services/{service_name}
-#         #         cp base4service_template/rename.sh {project_root}/src/services/{service_name}
-#         #         cp base4service_template/tests/test_template.py {project_root}/tests/test_{service_name}.py
-# 		# 		rm -rf base4service_template
-# 		# 		cd {project_root}/src/services/{service_name}
-# 		# 		bash rename.sh {service_name}
-# 		# 		rm  {project_root}/src/services/{service_name}/rename.sh
-# 		# 		mv {project_root}/src/services/{service_name}/yaml_sources/model.yaml {project_root}/src/services/{service_name}/yaml_sources/{service_name}_model.yaml
-# 		# 		mv {project_root}/src/services/{service_name}/yaml_sources/schema.yaml {project_root}/src/services/{service_name}/yaml_sources/{service_name}_schema.yaml
-# 		# 		'''
-#         #         )
-#         #         print(f'[*] service -> {service_name} created!')
-#         #
-#         #     else:
-#         #         print(f'[*] please choose template')
-#         #         for i, j in enumerate(existing_service_templates, start=1):
-#         #             print(f'->: {j}')
-#         #         return
-#         #
-#         #     # generate main config yaml
-#         #     compile_main_config(service_name, gen_items=gen_type.split(','))
-#         #
-#         #     # continue with another command
-#         #     command = 'compile-yaml'
-#
-#     if command == 'compile-yaml':
-#
-#         try:
-#             _yaml_file = (project_root + '/config/' + yaml_file) if '/' not in yaml_file else yaml_file
-#             with open(_yaml_file) as f:
-#                 data = yaml.safe_load(f)
-#         except Exception as e:
-#             print(f'Error loading {_yaml_file}')
-#             print(e)
-#             return
-#
-#         if data and 'services' in data and isinstance(data['services'], list):
-#             for i in data['services']:
-#                 svc_name = i['name']
-#
-#                 if service_name and svc_name not in service_name:
-#                     continue
-#
-#                 location = i['location']
-#
-#                 if not gen_type:
-#                     to_gen = i.get('gen')
-#                 else:
-#                     to_gen = gen_type
-#
-#                 gen4svc(svc_name, location, gen=to_gen)
-#
-# # if __name__ == '__main__':
-# #     do()
+    :param get_project_root(): Path to the PyCharm project.
+    :param source_dirs: List of source directories.
+    :param tests_dir: Name of the directory for tests.
+    """
+    # Validate the project path
+    if not os.path.exists(get_project_root()):
+        raise FileNotFoundError(f"Project path '{get_project_root()}' does not exist.")
+    
+    if not os.path.isdir(get_project_root()):
+        raise NotADirectoryError(f"Project path '{get_project_root()}' is not a directory.")
+    
+    # Ensure the .idea directory exists
+    idea_path = os.path.join(get_project_root(), ".idea")
+    if not os.path.exists(idea_path):
+        os.makedirs(idea_path)
+        print(f"Created '.idea' directory for project: {get_project_root()}")
+    
+    # Validate and create source directories
+    for source_dir in ['lib/base4', 'src']:
+        source_path = os.path.abspath(os.path.join(get_project_root(), source_dir))
+        if not os.path.exists(source_path):
+            print(f"Source directory '{source_path}' does not exist. Creating it.")
+            os.makedirs(source_path, exist_ok=True)
+    
+    # Validate and create the tests directory
+    tests_path = os.path.abspath(os.path.join(get_project_root(), 'tests'))
+    if not os.path.exists(tests_path):
+        print(f"Test directory '{tests_path}' does not exist. Creating it.")
+        os.makedirs(tests_path, exist_ok=True)
+    
+    # Create the root XML element
+    project_root = ET.Element("project", version="4")
+    component = ET.SubElement(project_root, "component", name="ProjectRootManager", version="2")
+    content = ET.SubElement(component, "content", url=f"file://{os.path.abspath(get_project_root())}")
+    
+    # Add each source directory to the XML configuration
+    for source_dir in ['lib/base4', 'src']:
+        source_path = os.path.abspath(os.path.join(get_project_root(), source_dir))
+        ET.SubElement(content, "sourceFolder", url=f"file://{source_path}", isTestSource="false")
+    
+    # Add test directory as a test source folder
+    ET.SubElement(content, "sourceFolder", url=f"file://{tests_path}", isTestSource="true")
+    
+    # Optionally exclude specific directories (e.g., virtual environments)
+    excluded_path = os.path.join(get_project_root(), "venv")
+    if os.path.exists(excluded_path):
+        ET.SubElement(content, "excludeFolder", url=f"file://{os.path.abspath(excluded_path)}")
+    
+    # Add output directory
+    ET.SubElement(component, "output", url=f"file://{os.path.join(get_project_root(), 'out')}")
+    
+    # Generate the XML configuration file
+    config_path = os.path.join(idea_path, "misc.xml")
+    tree = ET.ElementTree(project_root)
+    
+    with open(config_path, "wb") as xml_file:
+        tree.write(xml_file, encoding="UTF-8", xml_declaration=True)
+    
+    print(f"PyCharm configuration successfully created at: {config_path}")
