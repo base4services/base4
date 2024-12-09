801b14f296b17f5208bb9f8d8a730f6f9f55f17e
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-12-09 18:53:47 +0100

    n

--- base4/utilities/config.py
+++ base4/utilities/config.py
@@ -67,7 +67,6 @@ def yaml_to_env(yaml_cfg):
             if key == 'db_postgres_databases':
                 continue
             env_file.write(f"{key.upper()}={value}\n")
-        env_file.write(f"## PROJECT DATABASES:\n")
         env_file.write("DB_TEST=test_${DB_PREFIX}\n")
         for p in flat_config['db_postgres_databases'].split(','):
             env_file.write("DB_%s=${DB_PREFIX}\n" % (p.upper(),))
