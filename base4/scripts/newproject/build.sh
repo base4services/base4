3d3f77a9b8bab18548125d70479ad1055d0015b3
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2024-12-30 11:02:08 +0100

    db

--- base4/scripts/newproject/build.sh
+++ base4/scripts/newproject/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# new-project script
 base64_encoded_data=$(base64 -i new-project.sh)
-
 echo "$base64_encoded_data" > build.txt
 scp build.txt websites.prod:/home/digital/websites/base4/project.sh
 rm build.txt
 
-#alias newproject='curl -sS https://dcadmin:123@base4.digitalcube.dev/project.sh | base64 --decode | bash -s --'
+# init-project script
+base64_encoded_data=$(base64 -i init-project.sh)
+echo "$base64_encoded_data" > build.txt
+scp build.txt websites.prod:/home/digital/websites/base4/init-project.sh
+rm build.txt
+
+
+
