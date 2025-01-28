7f44fb0733cb4a9fbdb649d027dc8dfec7a276ae
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2025-01-28 09:52:58 +0100

    sredjeno

--- base4/utilities/db/base.py
+++ base4/utilities/db/base.py
@@ -104,7 +104,7 @@ class BaseServiceDbUtils:
             update_if_exists_key_fields = base_service_instance.schema.check_existence_rules()[key]
             existence_rule = base_service_instance.schema.check_existence_rules()[key]
             update_if_exists_value_fields = [getattr(list_item, fld) for fld in existence_rule]
-            return await service_loc[key](request).create(
+            return await service_loc[key]().create(
                 logged_user_id,
                 list_item,
                 request,
@@ -115,12 +115,7 @@ class BaseServiceDbUtils:
                 return_db_object=True,
             )
         else:
-            try:
-                return await service_loc[key](request).create(# logged_user_id,
-                                                              list_item, request, **list_item.unq(), return_db_object=True)
-            except Exception as e:
-                raise
-            ...
+            return await service_loc[key]().create(logged_user_id, list_item, request, **list_item.unq(), return_db_object=True)
 
     @staticmethod
     async def _create_and_save_item(base_service_instance, body: dict, logged_user_id: uuid.UUID, _conn):
