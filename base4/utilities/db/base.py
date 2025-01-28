907f242fede3512f8e27a156e99a2f9b48742ff0
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2025-01-28 17:06:59 +0100

    v4 refactor

--- base4/utilities/db/base.py
+++ base4/utilities/db/base.py
@@ -100,12 +100,11 @@ class BaseServiceDbUtils:
         base_service_instance, key: str, list_item: Any, service_loc: Dict, logged_user_id: uuid.UUID, request: Request, _conn
     ):
         """Create or update a related item based on existence rules."""
-
         if key in base_service_instance.schema.check_existence_rules():
             update_if_exists_key_fields = base_service_instance.schema.check_existence_rules()[key]
             existence_rule = base_service_instance.schema.check_existence_rules()[key]
             update_if_exists_value_fields = [getattr(list_item, fld) for fld in existence_rule]
-            return await service_loc[key]().create(
+            return await service_loc[key](request).create(
                 logged_user_id,
                 list_item,
                 request,
@@ -116,7 +115,12 @@ class BaseServiceDbUtils:
                 return_db_object=True,
             )
         else:
-            return await service_loc[key]().create(logged_user_id, list_item, request, **list_item.unq(), return_db_object=True)
+            try:
+                return await service_loc[key](request).create(# logged_user_id,
+                                                              list_item, request, **list_item.unq(), return_db_object=True)
+            except Exception as e:
+                raise
+            ...
 
     @staticmethod
     async def _create_and_save_item(base_service_instance, body: dict, logged_user_id: uuid.UUID, _conn):
