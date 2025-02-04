25712c2945c84c8ee8a282aa26606d5c1dd38047
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2025-02-04 14:59:00 +0100

    docker configuration

--- base4/scripts/newproject/init-project.sh
+++ base4/scripts/newproject/init-project.sh
@@ -133,7 +133,7 @@ echo "$current_folder/src" >> "$site_packages/pythonpaths.pth"
 echo "[*] Upgrading pip..."
 pip3 install --upgrade pip -q
 echo "[*] Installing project requirements.txt..."
-pip3 install -r requirements.txt -q
+pip3 install -r requirements.txt
 
 
 if [ "$install_type" == "lib" ]; then
