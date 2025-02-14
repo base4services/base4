4f4b57f1fba5551635f47389dce9123a9e3472cb
d3v3l0p3r90 <nemanja.d.adamov@gmail.com>
2025-02-14 15:54:05 +0100

    n

--- base4/utilities/ws.py
+++ base4/utilities/ws.py
@@ -8,7 +8,6 @@ base_dotenv.load_dotenv()
 
 SIO_REDIS_HOST = os.getenv('SOCKETIO_REDIS_HOST')
 SIO_REDIS_PORT = os.getenv('SOCKETIO_REDIS_PORT')
-HOST = os.getenv('SOCKETIO_HOST')
 
 
 def sio_client_manager(write_only=False):
@@ -25,10 +24,7 @@ def extract_domain(host):
 
 
 sio_connection = sio_client_manager(write_only=False)
-
-
 async def emit(event, data=None, room=None, connection=None):
-    ns = extract_domain(HOST) + '='
     if connection:
-        return await connection.emit(event=event, data=data, room=ns + room if room else None)
-    return await sio_connection.emit(event=event, data=data, room=ns + room if room else None)
+        return await connection.emit(event=event, data=data, room=room if room else None)
+    return await sio_connection.emit(event=event, data=data, room=room if room else None)
